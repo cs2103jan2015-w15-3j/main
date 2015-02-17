2904657adc3b9abe6b06836bc63a4041886165d7
Kai Lin <kailin.t@gmail.com>
2015-02-17 17:35:14 +0800

    implemented Command class

--- src/Command.java
+++ src/Command.java
@@ -1,12 +1,31 @@
 
 public class Command {
-    public enum TYPE {
-        ADD, MARK, DELETE, SEARCH, EDIT, DISPLAY, UNDO;
+	
+	public TYPE type;
+	public KeyParamPair[] paramPairArray;
+	
+	public enum TYPE {
+        ADD, MARK, DELETE, SEARCH, EDIT, DISPLAY, UNDO, ERROR;
     }
+    
+    public Command(TYPE cType, KeyParamPair[] pairArray) {
+    	type = cType;
+    	paramPairArray = pairArray;
+	}
+    
+    public TYPE getType() {
+		return type;
+	}
 
-    public TYPE type;
+	public void setType(TYPE type) {
+		this.type = type;
+	}
 
-    public void execute() {
+	public KeyParamPair[] getParamPairArray() {
+		return paramPairArray;
+	}
+	
+	public void execute() {
         // TODO Auto-generated method stub
 
     }
