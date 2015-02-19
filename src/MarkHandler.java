b26026891ab4d8d3d0a5d575590cd884cbd8f007
Zhu Liang <zhuliangg11@gmail.com>
2015-02-19 22:51:26 +0800

    set up basic structure for mark handler

--- src/MarkHandler.java
+++ src/MarkHandler.java
@@ -3,8 +3,16 @@ public class MarkHandler implements CommandHandler {
 
 	@Override
 	public Signal process(Command c) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        KeyParamPair[] pairs = c.getParamPairArray();
 
+        // Ensure that there is only pair of key and params
+        if (pairs.length > 1) {
+            return new Signal(Signal.SIGNAL_INVALID_PARAMS);
+        } else {
+            // Real index equals to the index in command minus 1
+            int indexToMark = Integer.parseInt(pairs[0].getParam()) + 1;
+            // Todo todoToMark =
+            return null;
+        }
+	}
 }
