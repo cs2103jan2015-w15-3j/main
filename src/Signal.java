090a3150b2edc24c4dc3eb78a48722a9ecba4894
Zhu Liang <zhuliangg11@gmail.com>
2015-02-19 21:49:01 +0800

    implemented basic signal class

--- src/Signal.java
+++ src/Signal.java
@@ -1,4 +1,42 @@
-
+/**
+ * The Signal Class is used for constructing a signal as a feedback mechanism
+ * for interactions between different processes
+ * 
+ * The signals indicates whether an operation is successful or not.
+ * 
+ * It also contains parameters for displaying purposes, which are to be parsed
+ * and printed to the screen using {@link DisplayHandler}.
+ * 
+ * @author paradite
+ *
+ */
 public class Signal {
 	
+    public static final int SIGNAL_ERROR = -1;
+    public static final int SIGNAL_SUCCESS = 1;
+    public static final int SIGNAL_NOT_FOUND = 404;
+    public static final int SIGNAL_INVALID_COMMAND = -2;
+    public static final int SIGNAL_INVALID_PARAMS = -3;
+
+    private int type;
+    private String[] params;
+
+    public Signal(int type, String[] params) {
+        this.type = type;
+        this.params = params;
+    }
+
+    public Signal(int type) {
+        this.type = type;
+        this.params = null;
+    }
+
+    public int getType() {
+        return this.type;
+    }
+
+    public String[] getParams() {
+        return this.params;
+    }
+
 }
