28b409eefa97014e1f51e7ebd6f6e503cee2c72c
Jonathan Lim <jonathanlimsc92@gmail.com>
2015-02-27 19:19:20 +0800

    Implemented MarkHandler class. Added set method in Memory class and fixed add method bug (Used to add before saving).

--- src/MarkHandler.java
+++ src/MarkHandler.java
@@ -1,17 +1,40 @@
+/**
+ * This class handles all user input with "mark" as the first keyword with the format of mark <index>.
+ * It retrieves a Todo object from memory at the given index, marks the Todo as done and replaces the 
+ * existing copy in memory.
+ * 
+ * @author Jonathan Lim Siu Chi || ign3sc3nc3
+ * 
+ */
+import java.util.ArrayList;
 
 public class MarkHandler {
 
-	public static Signal process(ParsedInput c, Memory memory) {
-        KeyParamPair[] pairs = c.getParamPairArray();
+	public static Signal process(ParsedInput input, Memory memory) {
+		ArrayList<KeyParamPair> inputList = input.getParamPairList();
 
-        // Ensure that there is only pair of key and params
-        if (pairs.length > 1) {
-            return new Signal(Signal.SIGNAL_INVALID_PARAMS);
-        } else {
-            // Real index equals to the index in command minus 1
-            int indexToMark = Integer.parseInt(pairs[0].getParam()) + 1;
-            // Todo todoToMark =
-            return null;
-        }
+		// Ensure that there is only one KeyParamPair in inputList
+		if (inputList.size() > 1) {
+			return new Signal(Signal.SIGNAL_INVALID_PARAMS);
+		}
+
+		try {
+
+			// -1 discrepancy between user input index and index in memory is
+			// handled in Memory class
+			int index = Integer.parseInt(inputList.get(0).getParam());
+
+			Todo todoToMark = memory.get(index);
+			todoToMark.isDone = true;
+
+			// replace Todo in memory with marked Todo
+			memory.set(index, todoToMark);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Signal(Signal.SIGNAL_ERROR);
+		}
+
+		return new Signal(Signal.SIGNAL_SUCCESS);
 	}
 }
