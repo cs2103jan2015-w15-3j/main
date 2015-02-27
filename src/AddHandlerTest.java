087822491f995e982363c2d5d6f1f03337f311f2
Jonathan Lim <jonathanlimsc92@gmail.com>
2015-02-27 17:18:04 +0800

    Implemented AddHandlerTest to test Joda DateTime class

---
+++ src/AddHandlerTest.java
@@ -0,0 +1,11 @@
+import org.joda.time.DateTime;
+
+
+public class AddHandlerTest {
+	
+	public static void main(String[] args){
+		DateTime dt = new DateTime();
+		System.out.println(dt);
+		System.out.println(DateTime.now());
+	}
+}
