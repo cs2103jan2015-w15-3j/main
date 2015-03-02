2c59570fb7977a0d0d2d10bb7f6e57afecce9563
Kai Lin <kailin.t@gmail.com>
2015-03-02 19:55:29 +0800

    added try and catch block for parseInt in DeleteHandler.
    returns SIGNAL_INVALID_PARAM with NumberFormatException is caught

--- src/com/equinox/DeleteHandler.java
+++ src/com/equinox/DeleteHandler.java
@@ -12,12 +12,18 @@ public class DeleteHandler {
 		
 		ArrayList<KeyParamPair> paramPairList = input.getParamPairList();
 		//Check for valid number of keywords
-    	int numberOfKeywords = input.getParamPairList().size();
+    	int numberOfKeywords = paramPairList.size();
     	if(numberOfKeywords > 1){
     		return new Signal(Signal.SIGNAL_INVALID_PARAMS);
     	}
     	
-    	int deleteIndex = Integer.parseInt(paramPairList.get(0).getParam());
+    	int deleteIndex;
+		try {
+			deleteIndex = Integer.parseInt(paramPairList.get(0).getParam());
+		} catch (NumberFormatException e1) {
+			return new Signal(Signal.SIGNAL_INVALID_PARAMS);
+		}
+		
     	try {
     		memory.remove(deleteIndex);
     	} catch (IndexOutOfBoundsException e) {
