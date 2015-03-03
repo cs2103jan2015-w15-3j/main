97ef49fea0b6fdb832d70a3b89ef2890d85e6a79
IkarusWill <qwerty.willy@gmail.com>
2015-03-03 12:44:29 +0800

    DateParser now throws DateUndefinedException to be caught in the respective handlers and returned as Signals

---
+++ src/com/equinox/DateUndefinedException.java
@@ -0,0 +1,13 @@
+package com.equinox;
+
+public class DateUndefinedException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public DateUndefinedException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public DateUndefinedException(String message) {
+		super(message);
+	}
+}
