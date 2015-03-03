d2a5c25fdcc442a51c31c2d1e05d6b9c0fccbaa8
IkarusWill <qwerty.willy@gmail.com>
2015-03-04 04:29:13 +0800

    Changed implementation of stack to LinkedList. Defined Exception for cases where no states can be restored to. Offloaded size checks from UndoHandler. Updated MemoryTest.

--- src/com/equinox/Memory.java
+++ src/com/equinox/Memory.java
@@ -1,20 +1,19 @@
 package com.equinox;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 public class Memory {
 	private static int STACK_MAX_SIZE = 5;
 	private ArrayList<Todo> currentState;
-	private ArrayList<ArrayList<Todo>> undoStack;
-	private ArrayList<ArrayList<Todo>> redoStack;
-	private int undoStackIndex;
-	private int redoStackIndex;
+	private LinkedList<ArrayList<Todo>> undoStack;
+	private LinkedList<ArrayList<Todo>> redoStack;
 
 	public Memory() {
 		this.currentState = new ArrayList<Todo>();
-		this.undoStack = new ArrayList<ArrayList<Todo>>();
-		this.redoStack = new ArrayList<ArrayList<Todo>>();
-		this.undoStackIndex = 0;
-		this.redoStackIndex = 0;
+		this.undoStack = new LinkedList<ArrayList<Todo>>();
+		this.redoStack = new LinkedList<ArrayList<Todo>>();
 	}
 
 	public void add(Todo todo) {
@@ -24,27 +23,25 @@ public class Memory {
 	public Todo get(int userIndex) {
 		return currentState.get(--userIndex);
 	}
-	
+
 	public int size() {
 		return currentState.size();
 	}
-	
-	public int stackSize(){
+
+	public int stackSize() {
 		return undoStack.size();
 	}
-	
+
 	public void remove(int userIndex) {
 		currentState.remove(--userIndex);
 	}
 
 	public void saveCurrentState() {
 		ArrayList<Todo> currentStateCopy = deepCopyCurrentState();
-		if(undoStack.size() <= STACK_MAX_SIZE) {
-			undoStack.add(currentStateCopy);
-		} else {
-			undoStack.set(undoStackIndex, currentStateCopy);
-		}	
-		undoStackIndex = incrementModulo(undoStackIndex);
+		if (undoStack.size() > STACK_MAX_SIZE) {
+			undoStack.removeFirst();
+		}
+		undoStack.add(currentStateCopy);
 	}
 
 	/**
@@ -52,51 +49,41 @@ public class Memory {
 	 */
 	private ArrayList<Todo> deepCopyCurrentState() {
 		ArrayList<Todo> currentStateCopy = new ArrayList<Todo>();
-		for(Todo todo: currentState) {
+		for (Todo todo : currentState) {
 			currentStateCopy.add(new Todo(todo));
 		}
 		return currentStateCopy;
 	}
 
-	public void restoreHistoryState() {
+	public void restoreHistoryState() throws StateUndefinedException {
 		ArrayList<Todo> currentStateCopy = deepCopyCurrentState();
-		if(redoStack.size() <= STACK_MAX_SIZE) {
-			redoStack.add(currentStateCopy);
-		} else {
-			redoStack.set(redoStackIndex, currentStateCopy);
+		if (redoStack.size() > STACK_MAX_SIZE) {
+			redoStack.removeFirst();
 		}
-		redoStackIndex = incrementModulo(redoStackIndex);
-		undoStackIndex = decrementModulo(undoStackIndex);
-		
-		currentState = undoStack.get(undoStackIndex);
-	}
-	
-	public void restoreFutureState() {
-		saveCurrentState();
-		redoStackIndex = decrementModulo(redoStackIndex);
-		currentState = redoStack.get(redoStackIndex);
+		redoStack.add(currentStateCopy);
+
+		try {
+			currentState = undoStack.removeLast();
+		} catch (NoSuchElementException e) {
+			throw new StateUndefinedException("No history states exist.");
+		}
 	}
 
-    /**
-     * Method to get all the todos for displaying purposes
-     * 
-     * @return all todos as ArrayList<Todo>
-     */
-    public ArrayList<Todo> getAllTodos() {
-        return currentState;
-    }
-    
-    private static int incrementModulo(int value) {
-    	value++;
-    	value %= STACK_MAX_SIZE;
-    	return value;
-    }
-    
-    private static int decrementModulo(int value) {
-    	value--;
-    	if(value < 0) {
-    		value += STACK_MAX_SIZE;
-    	}
-    	return value;
-    }
+	public void restoreFutureState() throws StateUndefinedException {
+		saveCurrentState();
+		try {
+			currentState = redoStack.removeLast();
+		} catch (NoSuchElementException e) {
+			throw new StateUndefinedException("No future states exist.");
+		}
+	}
+
+	/**
+	 * Method to get all the todos for displaying purposes
+	 * 
+	 * @return all todos as ArrayList<Todo>
+	 */
+	public ArrayList<Todo> getAllTodos() {
+		return currentState;
+	}
 }
