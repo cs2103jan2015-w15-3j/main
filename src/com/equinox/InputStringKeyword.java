ffbbdc14ab3bb749fb9abb1dcc63f9bf1a092723
IkarusWill <qwerty.willy@gmail.com>
2015-03-03 15:39:09 +0800

    Moved KEYWORDS enum out as a class. InputStringKeyword now checks for commands.

--- src/com/equinox/InputStringKeyword.java
+++ src/com/equinox/InputStringKeyword.java
@@ -1,31 +1,42 @@
 package com.equinox;
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class InputStringKeyword {
+	
+	private static final String KEY_ADD = "add";
+	private static final String KEY_MARK = "mark";
+	private static final String KEY_DELETE = "delete";
+	private static final String KEY_SEARCH = "search";
+	private static final String KEY_EDIT = "edit";
+	private static final String KEY_DISPLAY = "display";
+	private static final String KEY_UNDO = "undo";
+	private static final String KEY_BY = "by";
+	private static final String KEY_FROM = "from";
+	private static final String KEY_TO = "to";
+	private static final String KEY_ON = "on";
+	private static final String KEY_AT = "at";
 
-    public static final String KEY_ADD = "add";
-    public static final String KEY_DELETE = "delete";
-    public static final String KEY_ON = "on";
-    public static final String KEY_AT = "at";
-    public static final String KEY_MARK = "mark";
-
-    public enum KEYWORDS {
-        ADD, MARK, DELETE, SEARCH, EDIT, DISPLAY, UNDO, ON, AT;
-    }
-
-    public static HashMap<String, KEYWORDS> keywords;
+    private static Map<String, KEYWORDS> keywords;
+    private static Map<String, KEYWORDS> commands;
 
     static {
-        keywords = new HashMap<String, InputStringKeyword.KEYWORDS>();
-        keywords.put(KEY_ADD, KEYWORDS.ADD);
-        keywords.put(KEY_DELETE, KEYWORDS.DELETE);
+    	commands = new HashMap<String, KEYWORDS>();
+    	commands.put(KEY_ADD, KEYWORDS.ADD);
+        commands.put(KEY_MARK, KEYWORDS.MARK);
+        commands.put(KEY_DELETE, KEYWORDS.DELETE);
+        commands.put(KEY_SEARCH, KEYWORDS.SEARCH);
+        commands.put(KEY_EDIT, KEYWORDS.EDIT);
+        commands.put(KEY_DISPLAY, KEYWORDS.DISPLAY);
+        commands.put(KEY_UNDO, KEYWORDS.UNDO);
+    	
+        keywords = new HashMap<String, KEYWORDS>(commands);
+        keywords.put(KEY_BY, KEYWORDS.BY);
+        keywords.put(KEY_FROM, KEYWORDS.FROM);
+        keywords.put(KEY_TO, KEYWORDS.TO);
         keywords.put(KEY_ON, KEYWORDS.ON);
         keywords.put(KEY_AT, KEYWORDS.AT);
-        keywords.put(KEY_MARK, KEYWORDS.MARK);
-    }
-
-    public InputStringKeyword() {
     }
 
     public static boolean isKeyword(String s) {
@@ -34,5 +45,13 @@ public class InputStringKeyword {
 
     public static KEYWORDS getKeyword(String s) {
         return keywords.get(s);
+    }
+    
+    public static boolean isCommand(String s) {
+    	return commands.containsKey(s);
+    }
+    
+    public static KEYWORDS getCommand(String s) {
+    	return commands.get(s);
     }
 }
---
+++ src/com/equinox/KEYWORDS.java
@@ -0,0 +1,16 @@
+package com.equinox;
+
+public enum KEYWORDS {
+	ADD,
+	MARK,
+	DELETE,
+	SEARCH,
+	EDIT,
+	DISPLAY, 
+	UNDO, 
+	BY,
+	FROM,
+	TO,
+	ON,
+	AT;
+}
