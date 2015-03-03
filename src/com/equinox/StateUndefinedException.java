908cf1be9229be99c7cf6f64e5f5c1e3ac37da9f
IkarusWill <qwerty.willy@gmail.com>
2015-03-04 04:27:44 +0800

    Switched stack implementation to LinkedList. Implemented Exception for cases where Undo or Redo can find no state in the memory to restore to. Offloaded size checks from UndoHandler.

---
+++ src/com/equinox/StateUndefinedException.java
@@ -0,0 +1,11 @@
+package com.equinox;
+
+public class StateUndefinedException extends Exception {
+
+	public StateUndefinedException() {
+	}
+
+	public StateUndefinedException(String message) {
+		super(message);
+	}
+}
