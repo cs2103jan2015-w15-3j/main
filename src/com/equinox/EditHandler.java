2a8c5daf585e5a74e83dafa29c1baa41a0b1ccc5
IkarusWill <qwerty.willy@gmail.com>
2015-03-03 18:14:39 +0800

    Implemented EditHandler.

--- src/com/equinox/EditHandler.java
+++ src/com/equinox/EditHandler.java
@@ -1,10 +1,38 @@
 package com.equinox;
 
+import java.util.ArrayList;
+
 public class EditHandler {
 
-	public static Signal process(ParsedInput c, Memory memory) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Signal process(ParsedInput input, Memory memory) {
+		ArrayList<KeyParamPair> paramPairList = input.getParamPairList();
+		int userIndex = Integer.parseInt(paramPairList.get(0).getParam());
+		memory.saveCurrentState();
+		Todo editing = memory.get(userIndex);
+		
+		try {
+			for (int i = 1; i < paramPairList.size(); i++) {
+				String keyword = paramPairList.get(i).getKeyword();
+				String param = paramPairList.get(i).getParam();
+
+				switch (keyword) {
+				case "title":
+					editing.setTitle(param);
+					break;
+				case "start":
+					editing.setStartTime(DateParser.parseDate(param));
+					break;
+				case "end":
+					editing.setEndTime(DateParser.parseDate(param));
+					break;
+				case "done":
+					editing.setDone(Boolean.parseBoolean(param));
+				}
+			}
+		} catch (DateUndefinedException e) {
+			return new Signal(Signal.SIGNAL_INVALID_PARAMS);
+		}
+		return new Signal(Signal.SIGNAL_SUCCESS);
 	}
 
 }
