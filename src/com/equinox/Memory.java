a6e618e8914efa9920496366839fc27909221066
IkarusWill <qwerty.willy@gmail.com>
2015-03-05 00:00:56 +0800

    Resolved bugs with deletions and removal regarding undo and redo. Reimplemented test case. Tested and confirmed with unit test case.

--- src/com/equinox/Memory.java
+++ src/com/equinox/Memory.java
@@ -18,10 +18,16 @@ public class Memory {
 	}
 
 	public void add(Todo todo) {
+		save(todo.getPlaceholder());
 		memoryMap.put(todo.getIndex(), todo);
 	}
 
-	public Todo get(int index) {
+	public Todo get(int index) { // TODO Handle case where todo indicated by index does not exist.
+		return memoryMap.get(index);
+	}
+	
+	public Todo setterGet(int index) { // TODO Handle case where todo indicated by index does not exist.
+		save(memoryMap.get(index));
 		return memoryMap.get(index);
 	}
 
@@ -29,52 +35,74 @@ public class Memory {
 		return memoryMap.size();
 	}
 
-	public void remove(int index) {
+	public void remove(int index) { // TODO Handle case where todo indicated by index does not exist.
+		save(memoryMap.get(index));
 		memoryMap.remove(index);
 	}
 
 	public void save(Todo toBeSaved) {
-		Todo toBeSavedCopy = new Todo(toBeSaved);
-		if (undoStack.size() > STACK_MAX_SIZE) {
-			undoStack.removeFirst();
+		if(toBeSaved == null) {
+			undoStack.add(null);
+		} else {
+			Todo toBeSavedCopy = new Todo(toBeSaved);
+			if (undoStack.size() > STACK_MAX_SIZE) {
+				undoStack.removeFirst();
+			}
+			undoStack.add(toBeSavedCopy);
 		}
-		undoStack.add(toBeSavedCopy);
 	}
 
 	public void restoreHistoryState() throws StateUndefinedException {
-		Todo toReplace;
+		Todo fromStack;
 		try {
-			toReplace = undoStack.removeLast();
+			fromStack = undoStack.removeLast();
 		} catch (NoSuchElementException e) {
 			throw new StateUndefinedException("No history states exist.");
 		}
 		
-		int index = toReplace.getIndex();
-		Todo toBeReplaced = memoryMap.get(index);
+		int index = fromStack.getIndex();
+		Todo inMemory = memoryMap.get(index);
+		
+		if(inMemory == null) {
+			inMemory = fromStack.getPlaceholder();
+		}
 		
 		if (redoStack.size() > STACK_MAX_SIZE) {
 			redoStack.removeFirst();
 		}
-		redoStack.add(toBeReplaced);
-		memoryMap.put(index, toReplace);
+		redoStack.add(inMemory);
+		if(fromStack.getCreatedOn() == null) {
+			memoryMap.put(index, null);
+		} else {
+			memoryMap.put(index, fromStack);
+		}
 	}
 
 	public void restoreFutureState() throws StateUndefinedException {
-		Todo toReplace;
+		Todo fromStack;
 		try {
-			toReplace = redoStack.removeLast();
+			fromStack = redoStack.removeLast();
 		} catch (NoSuchElementException e) {
 			throw new StateUndefinedException("No future states exist.");
 		}
 		
-		int index = toReplace.getIndex();
-		Todo toBeReplaced = memoryMap.get(index);
+		int index = fromStack.getIndex();
+		Todo inMemory = memoryMap.get(index);
+		
+		if(inMemory == null) {
+			inMemory = fromStack.getPlaceholder();
+		} 
 		
 		if (undoStack.size() > STACK_MAX_SIZE) {
 			undoStack.removeFirst();
 		}
-		undoStack.add(toBeReplaced);
-		memoryMap.put(index, toReplace);
+		undoStack.add(inMemory);
+		
+		if (fromStack.getCreatedOn() == null) {
+			memoryMap.put(index, null);
+		} else {
+			memoryMap.put(index, fromStack);
+		}
 	}
 
 	/**
