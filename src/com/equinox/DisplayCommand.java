6a33186376bb37fdd94ea5b7dcc7ef7120ab5cd5
Zhu Liang <zhuliangg11@gmail.com>
2015-03-08 15:06:48 +0800

    refactored the display command to fit the new todo constructor

--- src/com/equinox/DisplayCommand.java
+++ src/com/equinox/DisplayCommand.java
@@ -10,8 +10,6 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-import com.joestelmach.natty.generated.DateParser;
-
 public class DisplayCommand extends Command {
 	
 	public DisplayCommand(ParsedInput input, Memory memory) {
@@ -191,34 +189,27 @@ public class DisplayCommand extends Command {
 			todos.add(new Todo("floating task"));
 
 			// Completed tasks
-			todos.add(new Todo("CS3230 deadline", DateParser
-					.parseDate("6 March at 9pm")));
+			todos.add(new Todo("CS3230 deadline", "6 March at 9pm"));
 
 			Todo todo_done_0 = new Todo("eat more");
 			todo_done_0.setDone(true);
 			todos.add(todo_done_0);
 
-			Todo todo_done = new Todo("CIP event",
-					DateParser.parseDate("3 March at 10am"),
-					DateParser.parseDate("3 March at 12pm"));
+			Todo todo_done = new Todo("CIP event","3 March at 10am to 3 March at 12pm");
 			todo_done.setDone(true);
 			todos.add(todo_done);
 
-			Todo todo_done_1 = new Todo("new year",
-					DateParser.parseDate("1 January at 10am"),
-					DateParser.parseDate("1 January at 11am"));
+			Todo todo_done_1 = new Todo("new year", "1 January at 10am to 1 January at 11am");
 			todo_done_1.setDone(true);
 			todos.add(todo_done_1);
 
-			Todo todo_done_2 = new Todo("CS1010 deadline",
-					DateParser.parseDate("3 Feb at 10pm"));
+			Todo todo_done_2 = new Todo("CS1010 deadline on 3 Feb at 10pm");
 			todo_done_2.setDone(true);
 			todos.add(todo_done_2);
 
 			todos.add(new Todo("read floating books"));
 
-			todos.add(new Todo("CS3243 project deadline", DateParser
-					.parseDate("7 March at 9am")));
+            todos.add(new Todo("CS3243 project deadline on 7 March at 9am"));
 		} catch (DateUndefinedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
