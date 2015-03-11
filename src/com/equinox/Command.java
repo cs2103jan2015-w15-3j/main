9d6cd01188774dbab57310bb86452092cd3fd441
IkarusWill <qwerty.willy@gmail.com>
2015-03-11 12:40:32 +0800

    Command abstract class now has a dateTimeList field.

--- src/com/equinox/Command.java
+++ src/com/equinox/Command.java
@@ -1,14 +1,19 @@
 package com.equinox;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.DateTime;
 
 public abstract class Command {
 	ParsedInput input;
 	Memory memory;
 	ArrayList<KeyParamPair> keyParamPairList;
+	List<DateTime> dateTimeList;
 	
 	public Command(ParsedInput input, Memory memory) {
-		keyParamPairList = input.getParamPairList();
+		this.keyParamPairList = input.getParamPairList();
+		this.dateTimeList = input.getDateTimeList();
 		this.input = input;
 		this.memory = memory;
 	}
