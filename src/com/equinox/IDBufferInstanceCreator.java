46da897c7d8657da4e8f555f6bccca6682e60e99
Jonathan Lim <jonathanlimsc92@gmail.com>
2015-03-17 21:36:15 +0800

    Added Javadoc comments for IDBufferInstanceCreator

--- src/com/equinox/IDBufferInstanceCreator.java
+++ src/com/equinox/IDBufferInstanceCreator.java
@@ -5,8 +5,21 @@ import java.lang.reflect.Type;
 import com.equinox.Memory.IDBuffer;
 import com.google.gson.InstanceCreator;
 
+/**
+ * Utility class that creates an instance of IDBuffer for Gson to
+ * serialise and deserialise the nested IDBuffer class in Memory 
+ * 
+ * @author Jonathan Lim Siu Chi || ign3sc3nc3
+ *
+ */
+
 public class IDBufferInstanceCreator implements InstanceCreator<Memory.IDBuffer>{
 
+	/**
+	 * Creates an instance of IDBuffer
+	 * 
+	 * @param Type 
+	 */
 	@Override
 	public IDBuffer createInstance(Type arg0) {
 		Memory memoryInstance = new Memory();
