5e973a18dc1910ea6047a8a82160b9fea7b4e0f5
Kai Lin <kailin.t@gmail.com>
2015-03-25 03:18:25 +0800

    modified parsing algorithm and included parsing for recurring tasks

--- src/com/equinox/exceptions/InvalidParamException.java
+++ src/com/equinox/exceptions/InvalidParamException.java
@@ -1,12 +1,11 @@
 package com.equinox.exceptions;
 
 public class InvalidParamException extends Exception {
-
 	private static final long serialVersionUID = 1L;
 
 	public InvalidParamException() {
 	}
-
+	
 	public InvalidParamException(String message) {
 		super(message);
 	}
---
+++ src/com/equinox/exceptions/InvalidPeriodException.java
@@ -0,0 +1,12 @@
+package com.equinox.exceptions;
+
+public class InvalidPeriodException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public InvalidPeriodException() {
+	}
+	
+	public InvalidPeriodException(String message) {
+		super(message);
+	}
+}
